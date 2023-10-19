cd65cc6d1ab43b391ac1e004389625d1ae854b74
LeoBlanco123 <rm99119@fiap.com.br>
2023-10-19 09:04:47 -0300

    layout root

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1>Home Page </h1>
+      <h1>HOME PAGE</h1>
 
-      <div>
-        <p><Link href="/produtos/tenis">Tênis...</Link></p>
-        <p><Link href="/produtos/produto/tenis-preto">Tênis Preto...</Link></p>
-        <p><Link href="/produtos/produto/camisa-xadrez">Camisa X...</Link></p>
-      </div>
     </>
   )
 }
