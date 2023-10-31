f5d61e2e1c991c0a5800865410eb54db98a2c772
LeoBlanco123 <rm99119@fiap.com.br>
2023-10-31 09:32:18 -0300

    get all and get by id

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -4,7 +4,7 @@ export default function Rodape() {
   return (
     <footer className="rodape">
         <div>
-            <p>Desenvolvido por: <span>pf0670</span></p>
+            <p>Desenvolvido por: <span>rm99119</span></p>
             <p>NEXT.js - React.js - GitHub - tailwindcss</p>
         </div>
     </footer> 
